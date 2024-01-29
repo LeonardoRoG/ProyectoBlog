c619627aed71da10a7d690eec63cce0db143e886
Leonardo <Leonardo@DESKTOP-119RNS3>
2024-01-29 09:52:10 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoFinal/Models/Publicacion.cs
@@ -0,0 +1,13 @@
+﻿namespace ProyectoFinal.Models
+{
+    public class Publicacion
+    {
+        public string Titulo { get; set; } = string.Empty; // Por defecto el valor es vacío, para quitar la advertencia de posible null
+        public string SubTitulo { get; set; } = string.Empty;
+        public string Creador { get; set; } = string.Empty;
+        public string Cuerpo { get; set; } = string.Empty;
+        public DateTime Creacion { get; set; } = DateTime.Now;
+        public string Imagen { get; set; } = string.Empty;
+
+    }
+}
---
+++ ProyectoFinal/Rules/PublicacionRule.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using ProyectoFinal.Models;
+using System.Data.SqlClient;
+
+namespace ProyectoFinal.Rules
+{
+    public class PublicacionRule
+    {
+        public Publicacion GetOnePostRandom()
+        {
+            var connectionString = @"Server=DESKTOP-119RNS3\SQLEXPRESS;Database=BlogDataBase;Trusted_Connection=True"; // Luego va a appsettings.json
+
+            using var connection = new SqlConnection(connectionString); // Usa System.Sql
+            {
+                // Abro la conexion y pido mediante una consulta con Dapper los datos que luego se transforman en el objeto
+                connection.Open();
+                var post = connection.Query<Publicacion>("SELECT TOP 1 * FROM Publicacion"); // Usando Dapper, devuelve lista IEnumerable
+
+                return post.First(); // Se coloca First() para que devuelva el primer item de la lista
+            }
+
+        }
+    }
+}
